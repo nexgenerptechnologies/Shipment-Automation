a42901003a6a592f5ae6dd1a0c08094d15fc1b09
Virendra Maurya <nexgenerptechnologies@gmail.com>
2026-05-04 21:38:01 +0530

    Cleaning hooks.py

--- hooks.py
+++
@@ -1,7 +0,0 @@
-# File 1: hooks.py
-app_name = "shipment_automation"
-app_title = "Shipment Automation"
-app_publisher = "NexGen ERP Technologies"
-app_description = "Automated Import Shipment Processing"
-app_email = "admin@nexgenerp.com"
-app_license = "mit"
