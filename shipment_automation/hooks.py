51e86d16d6d327392e9147a538ee8e8b48218ccb
Virendra Maurya <nexgenerptechnologies@gmail.com>
2026-05-04 21:03:29 +0530

    Uploading shipment_automation/hooks.py via API

--- shipment_automation/hooks.py
+++ shipment_automation/hooks.py
@@ -1,6 +1,8 @@
-app_name = 'shipment_automation'
-app_title = 'Shipment Automation'
-app_publisher = 'NexGen ERP Technologies'
-app_description = 'Automated Import Shipment Processing'
-app_email = 'admin@nexgenerp.com'
-app_license = 'mit'
+app_name = "shipment_automation"
+app_title = "Shipment Automation"
+app_publisher = "NexGen ERP Technologies"
+app_description = "Automated Import Shipment Processing"
+app_email = "admin@nexgenerp.com"
+app_license = "mit"
+
+app_include_js = "/assets/shipment_automation/js/shipment_automation.js"
