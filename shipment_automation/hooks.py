4cb0c03798ee13ef60b383a10ef446272d54a0d1
Virendra Maurya <nexgenerptechnologies@gmail.com>
2026-05-04 21:38:26 +0530

    Cleaning shipment_automation/hooks.py

--- shipment_automation/hooks.py
+++
@@ -1,9 +0,0 @@
-app_name = "shipment_automation"
-app_title = "Shipment Automation"
-app_publisher = "NexGen ERP Technologies"
-app_description = "Automated Import Shipment Processing"
-app_email = "admin@nexgenerp.com"
-app_license = "mit"
-
-# We removed app_include_js to prevent build errors on Frappe Cloud.
-# The DocType JS will load automatically from the doctype folder.
