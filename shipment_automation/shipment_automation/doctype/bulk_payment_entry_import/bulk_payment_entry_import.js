ffa0c3127e04139ab9edf91192a6301a3cac23af
Regis Digital <regissdigital@gmail.com>
2026-06-19 07:50:22 +0530

    fix: move Download Template button outside is_new check so it appears on new forms

--- shipment_automation/shipment_automation/doctype/bulk_payment_entry_import/bulk_payment_entry_import.js
+++ shipment_automation/shipment_automation/doctype/bulk_payment_entry_import/bulk_payment_entry_import.js
@@ -1,12 +1,12 @@
 frappe.ui.form.on("Bulk Payment Entry Import", {
     refresh: function(frm) {
+        frm.add_custom_button(__("Download Template"), function() {
+            window.open(
+                "/api/method/shipment_automation.shipment_automation.doctype.bulk_payment_entry_import.bulk_payment_entry_import.download_template"
+            );
+        }).addClass("btn-primary");
+        
         if (!frm.is_new()) {
-            frm.add_custom_button(__("Download Template"), function() {
-                window.open(
-                    "/api/method/shipment_automation.shipment_automation.doctype.bulk_payment_entry_import.bulk_payment_entry_import.download_template"
-                );
-            }).addClass("btn-primary");
-            
             if (frm.doc.status === "Draft" || frm.doc.status === "Failed") {
                 frm.add_custom_button(__("Start Validation"), function() {
                     frappe.call({
